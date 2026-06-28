7035e15a462de6da6715928263c3e617d077b13f
winkeyface14 <65014273+winkeyface14@users.noreply.github.com>
2026-06-28 16:40:05 +0800

    Initial port to 26.2 Fabric Needs more fixes

---
+++ src/main/java/net/winkeyface14/vanilla_expansion/TheVanillaExpansionDataGenerator.java
@@ -0,0 +1,11 @@
+package net.winkeyface14.vanilla_expansion;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheVanillaExpansionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
