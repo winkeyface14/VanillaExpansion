0cd439c0935b1ab95e2f8ca88192f46d0f0a56b5
winkeyface14 <65014273+winkeyface14@users.noreply.github.com>
2026-06-28 16:33:38 +0800

    Initial port to 26.2 Fabric Needs more fixes

---
+++ src/client/java/net/winkeyface14/vanilla_expansion/client/TheVanillaExpansionClient.java
@@ -0,0 +1,10 @@
+package net.winkeyface14.vanilla_expansion.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheVanillaExpansionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
